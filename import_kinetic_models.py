f9528c2eaaa0f8ef56e8298481a93354b9f1b2e5
Kian Mehrabani <kianmehrabani@gmail.com>
2022-12-28 22:31:01 +0000

    parallelize file downloads

--- import_kinetic_models.py
+++ import_kinetic_models.py
@@ -1,11 +1,16 @@
 import os
 from pathlib import Path
-from typing import Iterable, List, NamedTuple
+from typing import Iterable, List, NamedTuple, Type, Union
+from multiprocessing import Pool, cpu_count
 
 import requests
 from dotenv import load_dotenv
 from github import Github
 from github.ContentFile import ContentFile
+from github.Repository import Repository
+
+
+Seconds = Union[int, float]
 
 
 class DownloadPath(NamedTuple):
@@ -13,14 +18,22 @@ class DownloadPath(NamedTuple):
     download_url: str
 
 
-def download_rmg_models():
-    def get_paths(contents: List[ContentFile]) -> Iterable[DownloadPath]:
-        for content in contents:
-            if content.type == "dir":
-                yield from get_paths(repo.get_contents(content.path))
-            else:
-                yield DownloadPath(data_path / content.path, content.download_url)
+def get_paths(repo: Repository, data_path: Path, contents: List[ContentFile]) -> Iterable[DownloadPath]:
+    for content in contents:
+        if content.type == "dir":
+            yield from get_paths(repo, data_path, repo.get_contents(content.path))
+        else:
+            yield DownloadPath(data_path / content.path, content.download_url)
 
+
+def download(download_path: DownloadPath, timeout: Seconds = 10) -> None:
+    path, url = download_path
+    path.parent.mkdir(exist_ok=True, parents=True)
+    content = requests.get(url, timeout=timeout).content
+    path.write_bytes(content)
+
+
+def download_rmg_models():
     load_dotenv()
     PAT = os.getenv("PAT")
     g = Github(PAT)
@@ -29,9 +42,7 @@ def download_rmg_models():
     repo = g.get_repo(f"{owner_name}/{repo_name}")
     contents: list[ContentFile] = repo.get_contents("")
     data_path = Path("rmg-models")
-    paths = get_paths(contents)
+    paths = get_paths(repo, data_path, contents)
 
-    for path, url in paths:
-        path.parent.mkdir(exist_ok=True, parents=True)
-        content = requests.get(url, timeout=TIMEOUT).content
-        path.write_bytes(content)
+    thread_pool = Pool(cpu_count())
+    thread_pool.map(download, paths)
